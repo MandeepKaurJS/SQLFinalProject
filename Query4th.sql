a07bb6402de1e35504ebb39130503b9f78e6340b
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-22 16:14:48 -0700

    Add Query
    add one more query for assignment

---
+++ Query4th.sql
@@ -0,0 +1,20 @@
+use SQLFINALPROJECT
+Go
+create procedure GetDueDate
+as
+begin
+
+/****For each book that is loaned out from the "Sharpstown" 
+branch and whose DueDate is today, retrieve the book title, 
+the borrower's name, and the borrower's address. *****/
+
+select book1.Branchid,b2.BranchName,b1.BookTitle,n1.Borrower_name,n1.B_address 
+	from Book_loans book1
+		inner join Library_Branch b2 on book1.Branchid=
+		b2.BranchID
+		inner join Books b1 on book1.BookID=b1.BookID
+		inner join Borrower n1 on book1.Book_CardNo=
+		n1.CardNo
+		where BranchName='Sharpstown' and DateDue='2019-04-20'
+end
+execute[dbo].[GetDueDate]
