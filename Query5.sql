458f3f8c2e3706087bb81947df57db41f4335434
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-22 16:16:51 -0700

    Add Query 5
    Added one  more query

---
+++ Query5.sql
@@ -0,0 +1,15 @@
+use SQLFINALPROJECT
+Go
+create procedure CountBooks
+as 
+begin
+/**********For each library branch, retrieve the branch name and 
+the total number of books loaned out from that branch. *****/
+
+select b1.BranchName ,COUNT(*)as numberOfBooksOUt from 
+	Book_loans b2
+	inner join Library_Branch b1 on b1.Branchid=b2.BranchID
+	GROUP BY
+	b1.BranchName
+end
+execute[dbo].[CountBooks]
