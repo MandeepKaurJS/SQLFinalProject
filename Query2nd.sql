fde3c3ba8842a470b793f8ba375da9e6ca0bf215
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-23 11:45:16 -0700

    update Query
    Update query

--- Query2nd.sql
+++ Query2nd.sql
@@ -6,7 +6,7 @@ as
 Begin
 	/***How many copies of the book titled "The Lost Tribe"
  are owned by each library branch****/
- select count(Titles) as Title,BranchName from Book_copies
+ select count(*) as Title,BranchName from Book_copies
 inner join Library_Branch  on Book_copies.Branch_id=
 Library_Branch.BranchID where Titles='The Lost Tribe'
 group by BranchName
