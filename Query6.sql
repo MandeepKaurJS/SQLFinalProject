2e3f6512e1e6ee9db07b82a52a8dd6c949050899
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-23 12:56:07 -0700

    update query
    update query

--- Query6.sql
+++ Query6.sql
@@ -5,14 +5,12 @@ as
 begin
 /**********Retrieve the names, addresses, and the number of books 
 checked out for all borrowers who have more than five books checked out.***/
-	select count(Titles) as CheckedOutBooks,B_address,Borrower_name from 
+	select count(BookId) as CheckedOutBooks,B_address,Borrower_name from 
 	(Book_loans
 		inner join Borrower on Book_loans.Book_CardNo=
-		Borrower.CardNo
-		inner join Book_copies on Book_loans.BookID=
-		Book_copies.Book_ID)
-		group by B_Address,Titles,Borrower_name
-		having COUNT(Borrower_name)>5
+		Borrower.CardNo)
+		group by B_Address,Borrower_name
+		having COUNT(BookID)>5
 end
 Go
 execute[dbo].[countBorrower]
