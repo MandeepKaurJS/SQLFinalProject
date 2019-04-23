0b2abf98e4ac1d3a2c8ba6eb2c033933cf8ddd49
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-23 12:08:55 -0700

    update query
    update query

--- Query3rd.sql
+++ Query3rd.sql
@@ -6,9 +6,13 @@ as
 Begin
 	/*****Retrieve the names of all borrowers who do not have any books checked out****/
 
-select * from Borrower
-full outer join Book_loans on
-Book_loans.Book_CardNo=Borrower.CardNo
+	select *from Borrower
+	full outer join Book_loans on
+	Book_loans.Book_CardNo=Borrower.CardNo
+	where Book_loans.BookID is null or
+	CardNo is null
+	
 
 end 
+Go
 execute[dbo].[GetInfo]
